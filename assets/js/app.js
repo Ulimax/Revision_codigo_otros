54ecb8a4d6852e3931be57c0fa4b2a93f6aee398
Ulimax <uliortegao@gmail.com>
2024-03-22 11:51:18 -0600

    fix: errores de ubicación de carpetas y de sintaxis js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); // Agrege el punto .name
+const $b = document.querySelector('.blog'); // cambie el # por el punto .blog
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function  displayUser (username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();  // Agrege esta linea para transformar el json en objeto
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`   
 }
 
 displayUser('stolinski').catch(handleError);
